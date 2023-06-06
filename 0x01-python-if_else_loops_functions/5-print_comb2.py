b28a9cd40a4f965b2f8bc85d5d52f5486151082f
AropetJ <aropetjoel32@gmail.com>
2023-06-07 01:53:47 +0200

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# print_comb2.py
-# a program that prints numbers from 0 to 99
+# A program that prints numbers from 0 to 99
 for i in range(0, 100):
     if i == 99:
         print(f"{i}")
