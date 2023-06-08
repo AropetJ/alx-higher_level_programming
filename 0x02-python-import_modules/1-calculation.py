4b95362ad29bdade0b3ade89752dadb10e3225b4
AropetJ <aropetjoel32@gmail.com>
2023-06-08 18:42:35 +0200

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """ a program that imports functions from the
-    file calculator_1.py, does some Maths, and prints the result.
+    """ a program that imports functions from a
+    file, does some Maths, and prints the result.
     """
     from calculator_1 import sub, mul, div, add
 
-    a = 10
-    b = 5
+    Args:
+        a = 10
+        b = 5
 
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} + {} = {}".format(a, b, sub(a, b)))
