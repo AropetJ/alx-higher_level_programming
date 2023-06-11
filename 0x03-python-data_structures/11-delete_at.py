74ad8e28a864eaa292974bbc116aaf0930030ab5
AropetJ <aropetjoel32@gmail.com>
2023-06-11 23:53:47 +0200

    11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 11-delete_at.py
+# by: Aropet_Joel
+
+def delete_at(my_list=[], idx=0):
+        """A function that deletes the item at a specific position in a list."""
+        if idx >= 0 and idx < len(my_list):
+            del my_list[idx]
+        return my_list
+
