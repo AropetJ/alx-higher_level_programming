401dd6499587fe1e48d62260f6d280ce7a6d37ce
AropetJ <aropetjoel32@gmail.com>
2023-06-11 15:55:33 +0200

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """ A program that imports the variable a from the file variable_load_5.py"""
+    """ A program that imports the variable a
+    from the file variable_load_5.py
+    """
     from variable_load_5 import a
 
     print(a)
