cd94006ee1c18633ad2a3a03418106780d7736db
AropetJ <aropetjoel32@gmail.com>
2023-06-16 20:16:13 +0300

    11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-# 11-multiply_list_map.py
-# by: Aropet_Joel
-
 def multiply_list_map(my_list=[], number=0):
-    """A function that returns a list with all values multiplied by a number without using any loops"""
     return (list(map((lambda i: i * number), my_list)))
