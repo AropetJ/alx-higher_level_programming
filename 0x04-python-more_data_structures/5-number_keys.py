55e6e9f2088646e200abca3ea5ddd97c25778f86
AropetJ <aropetjoel32@gmail.com>
2023-06-16 19:42:57 +0300

    5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,11 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 # 5-number_keys.py
 # by: Aropet_Joel
 
 def number_keys(a_dictionary):
-    """A function that returns the number of keys in a dictionary."""
+    """A function that returns the number of
+    keys in a dictionary.
+    """
     i = 0
     list_the_keys = list(a_dictionary.keys())
 
