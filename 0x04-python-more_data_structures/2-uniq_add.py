bc80f924464754d6ebb5398e49c96a178798600e
AropetJ <aropetjoel32@gmail.com>
2023-06-16 17:37:34 +0300

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 2-uniq_add.py
+# by: Aropet_Joel
+
+def uniq_add(my_list=[]):
+    """A function that adds all unique integers in a list (only once for each integer)."""
+    uniq_list = set(my_list)
+    i = 0
+
+    for j in uniq_list:
+        i += j
+
+    return (i)
