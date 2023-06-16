7c7198f95045b63cba855ac11fbc6ada443c703b
AropetJ <aropetjoel32@gmail.com>
2023-06-16 17:24:53 +0300

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-square_matrix_simple.py
+# by: Aropet_Joel
+
+def square_matrix_simple(matrix=[]):
+    """A function that computes the square value of all integers of a matrix."""
+    nw_mtrx = matrix.copy()
+
+    for j in range(len(matrix)):
+        nw_mtrx[j] = list(map(lambda x: x**2, matrix[j]))
+
+    return (nw_mtrx)
