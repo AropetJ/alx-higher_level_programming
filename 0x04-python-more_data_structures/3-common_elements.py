cf7bae45f9c064bdbdd4d3fa5e88005546c2ee30
AropetJ <aropetjoel32@gmail.com>
2023-06-16 17:40:07 +0300

    3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 3-common_elements.py
+# by: Aropet_Joel
+
+def common_elements(set_1, set_2):
+    """A function that returns a set of common elements in two sets."""
+    return (set_1 & set_2)
