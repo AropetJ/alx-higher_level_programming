9aad12b1e59077ff08555c01d8f306ac5cce2ee3
AropetJ <aropetjoel32@gmail.com>
2023-06-16 18:38:34 +0300

    9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 9-multiply_by_2.py
+# by: Aropet_Joel
+
+def multiply_by_2(a_dictionary):
+        """A function that returns a new dictionary with all values multiplied by 2"""
+        new_dict = a_dictionary.copy()
+        list_the_keys = list(new_dict.keys())
+
+        for j in list_the_keys:
+            new_dict[j] *= 2
+
+        return (new_dict)
