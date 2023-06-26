3848300b67a9143f5376346974fdfdc68b6c76bc
AropetJ <aropetjoel32@gmail.com>
2023-06-26 22:22:32 +0300

    1-safe_print_integer.py2- change of format string and comment

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,14 +5,16 @@ def safe_print_integer(value):
     """A function that prints an integer with "{:d}".format().
 
     Args:
-        value (int, string, etc): An integer to be printed in the specified format.
+        value (int, string, etc): An integer to be printed in
+        the specified format.
 
     Returns:
         True if value has been correctly printed (it means the value is an integer).
         Otherwise, returns false.
     """
     try:
-        print("{:d}".format(value))
+        i = "{:d}"
+        print(i.format(value))
         return (True)
     except (TypeError, ValueError):
         return (False)
