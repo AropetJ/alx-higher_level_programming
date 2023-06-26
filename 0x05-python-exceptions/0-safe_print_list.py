98a9b927be09000969c92917b8f351475073b957
AropetJ <aropetjoel32@gmail.com>
2023-06-26 22:05:11 +0300

    0-safe_print_list.py-change on the variable i to j

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,7 +15,7 @@ def safe_print_list(my_list=[], x=0):
     for j in range(x):
         try:
             k = "{}"
-            print(k.format(my_list[i]), end="")
+            print(k.format(my_list[j]), end="")
             real_number += 1
         except IndexError:
             break
