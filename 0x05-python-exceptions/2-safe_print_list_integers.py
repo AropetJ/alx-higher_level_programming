8293ce0bc7923198915151a26c0155b57be8004d
AropetJ <aropetjoel32@gmail.com>
2023-06-26 22:37:18 +0300

    2-safe_print_list_integers.py3

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -13,8 +13,7 @@ def safe_print_list_integers(my_list=[], x=0):
     real_number = 0
     for i in range(0, x):
         try:
-            i = "{:d}"
-            print(i.format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             real_number += 1
         except (ValueError, TypeError):
             continue
