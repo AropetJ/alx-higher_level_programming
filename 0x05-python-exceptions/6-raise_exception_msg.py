be29654d576ac2f316715de03283854a382bf5a6
AropetJ <aropetjoel32@gmail.com>
2023-06-26 23:12:23 +0300

    6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-
+# 6-raise_exception_msg.py
 
 def raise_exception_msg(message=""):
-    """Raise a NameError exception with a message."""
+    """A function that raises a name exception with a message.
+
+    Args:
+        message="": The message to be printed
+    """
     raise NameError(message)
