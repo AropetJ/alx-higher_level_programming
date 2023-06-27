2df172069913bfc72db7ab3d65a79981d64ba50c
AropetJ <aropetjoel32@gmail.com>
2023-06-27 23:02:19 +0300

    4-square.py3

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# 4-square.py
+"""Define a class Square"""
+
+
+class Square():
+    """A class Square that defines a square by: (based on 2-square.py)"""
+
+    def __init__(self, size=0):
+        """Initializing the attributes of a square
+
+        Args:
+            size (int): Private instance attribute
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retriving the size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(size, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(size):
+        """Returns the current square area"""
+        return (size.__size ** 2)
