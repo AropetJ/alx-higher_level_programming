5a4f4710605f555f649761e0e3fe6d21af271501
AropetJ <aropetjoel32@gmail.com>
2023-07-03 22:54:31 +0300

    9-rectangle.py - raised some errors for extra cases

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -60,7 +60,9 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """ Returns a new Rectangle instance with width == height == size"""
-        if size <= 0:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
             raise ValueError("size must be > 0")
         return cls(size, size)
 
