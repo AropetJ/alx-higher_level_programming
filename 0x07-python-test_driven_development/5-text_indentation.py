0265199dd4c0808bf2e2abfc1ffe432f6c768bbf
AropetJ <aropetjoel32@gmail.com>
2023-07-05 02:25:42 +0300

    5-text_indentation.py - 2nd commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,19 +13,6 @@ def text_indentation(text):
     Raises:
         TypeError: text must be a string.
     """
-    #!/usr/bin/python3
-# 5-text_indentation.py
-"""Defines a text-indentation function."""
-
-
-def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
-    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
