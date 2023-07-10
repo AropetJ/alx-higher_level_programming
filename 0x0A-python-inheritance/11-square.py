cfb9a6a0e4e9dbefa36f90f6ab57fc0671c094d4
AropetJ <aropetjoel32@gmail.com>
2023-07-10 22:33:13 +0300

    11-square.py - first commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a subclass of Rectangle Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define a class Square"""
+
+    def __init__(self, size):
+        """Initialise the attributes of Square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
