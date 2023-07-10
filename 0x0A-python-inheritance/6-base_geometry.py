0de3a43a4be73a8ec37f9fd53fb6ce49ad6b5c83
AropetJ <aropetjoel32@gmail.com>
2023-07-10 21:27:47 +0300

    6-base_geometry.py - first commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """A class BaseGeometry."""
+
+    def area(self):
+        """Raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
