533a06ecbd203bf071903986955ab827628d246d
AropetJ <aropetjoel32@gmail.com>
2023-07-10 19:51:53 +0300

    0-lookup.py - checked and adjusted the file length

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# 0-lookup.py
 
 def lookup(obj):
     """ A function that returns the list of available
-    attributes and methods of an object
-    """
+    attributes and methods of an object"""
     return (dir(obj))
