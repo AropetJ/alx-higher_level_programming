31d3f34b96e163545f532d07cd007d3135b9a32e
AropetJ <aropetjoel32@gmail.com>
2023-07-10 19:59:29 +0300

    0-lookup.py - adjusted the pep error on blanck lines

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that looks up the attributes of an object attribute"""
 
+
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
     return (dir(obj))
