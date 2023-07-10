c4cca1ff9b02860b437c26c48908192b23feeb5d
AropetJ <aropetjoel32@gmail.com>
2023-07-10 22:22:53 +0300

    9-rectangle.py - first commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Defining the class rectangle"""
+
+    def __init__(self, width, height):
+        """Intialises the attributes of the class
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")
+
+    def __repr__(self):
+        return (self.__str__())
