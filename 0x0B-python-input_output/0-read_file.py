a54e2768f9e07a5a35d7132c04c78b8f34d78613
AropetJ <aropetjoel32@gmail.com>
2023-07-11 17:49:21 +0300

    0-read_file.py - added comments to the functions

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,10 @@
 
 
 def read_file(filename=""):
+    """Reads a text file(UTF-8) and prints it to stdout
+
+    Args:
+        filename (str): The text file to be read from.
+    """
     with open(filename, encoding="utf-8") as fPrint:
         print(fPrint.read(), end="")
