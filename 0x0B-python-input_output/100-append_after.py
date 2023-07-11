1ced0a7ba51a72f7243e8f5e8e2940a620365fab
AropetJ <aropetjoel32@gmail.com>
2023-07-11 20:18:37 +0300

    100-append_after.py - first commit

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,11 +10,11 @@ def append_after(filename="", search_string="", new_string=""):
         search_string (str): The string to search for within the file.
         new_string (str): The string to insert.
     """
-    text = ""
-    with open(filename) as r:
-        for line in r:
-            text += line
+    lines = []
+    with open(filename) as file:
+        for line in file:
+            lines.append(line)
             if search_string in line:
-                text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+                lines.append(new_string)
+    with open(filename, "w") as file:
+        file.write("".join(lines))
