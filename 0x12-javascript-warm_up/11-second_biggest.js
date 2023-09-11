4d6d588acd05e3c77d3721a23d91b1551be8bd49
AropetJ <aropetjoel32@gmail.com>
2023-09-12 01:12:10 +0300

    Completed task11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 const args = process.argv;
 
 if (args.length <= 3) {
-    console.log('0');
-  } else {
-    const arr = args.slice(2).map(Number);
-    const second = arr.sort(function (a, b) { return b - a; })[1];
-    console.log(second);
-  }
+  console.log('0');
+} else {
+  const arr = args.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
+}
