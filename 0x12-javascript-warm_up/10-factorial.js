b6f4e233e1ab042ad4f7a7f5931cdbf63667c8fb
AropetJ <aropetjoel32@gmail.com>
2023-09-11 23:56:40 +0300

    Completed task7 to task10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+function computeFactorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * computeFactorial(n - 1);
+}
+
+const arg1 = process.argv[2];
+
+const num = parseInt(arg1);
+
+const result = computeFactorial(num);
+
+console.log(result);
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+// Retrieve the first argument from process.argv
+const arg1 = process.argv[2];
+
+// Use parseInt to attempt conversion to an integer
+const numOccurrences = parseInt(arg1);
+
+// Check if numOccurrences is a valid integer (not NaN)
+if (!isNaN(numOccurrences)) {
+    // Use a loop to print "C is fun" the specified number of times
+    for (let i = 0; i < numOccurrences; i++) {
+        console.log("C is fun");
+    }
+} else {
+    console.log("Missing number of occurrences");
+}
