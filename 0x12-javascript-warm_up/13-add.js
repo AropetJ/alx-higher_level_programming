60dadce9e2d3a440662e2cef48e3abae005e6951
AropetJ <aropetjoel32@gmail.com>
2023-09-12 00:24:15 +0300

    Completed task11 to task13

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
 
-// Retrieve the first argument from process.argv
-const arg1 = process.argv[2];
-
-// Use parseInt to attempt conversion to an integer
-const numOccurrences = parseInt(arg1);
-
-// Check if numOccurrences is a valid integer (not NaN)
-if (!isNaN(numOccurrences)) {
-    // Use a loop to print "C is fun" the specified number of times
-    for (let i = 0; i < numOccurrences; i++) {
-        console.log("C is fun");
-    }
-} else {
-    console.log("Missing number of occurrences");
+function add(a, b) {
+    return a + b;
 }
+
+module.exports = {
+    add: add
+};
