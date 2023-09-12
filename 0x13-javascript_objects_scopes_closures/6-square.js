ce30d323b517ac686888a4aa0bed9d76571900a2
AropetJ <aropetjoel32@gmail.com>
2023-09-12 14:55:25 +0300

    completed task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+const baseSquare = require('./5-square');
+
+class Square extends baseSquare {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
