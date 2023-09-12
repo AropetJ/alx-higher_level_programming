4ac6778c921102f76b75771fdbe8da490c111f68
AropetJ <aropetjoel32@gmail.com>
2023-09-12 15:39:35 +0300

    completed task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const data = require('./101-data');
+const dict = data.dict;
+
+const occurrenceDict = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+
+  if (occurrences in occurrenceDict) {
+    occurrenceDict[occurrences].push(userId);
+  } else {
+    occurrenceDict[occurrences] = [userId];
+  }
+}
+
+console.log(occurrenceDict);
