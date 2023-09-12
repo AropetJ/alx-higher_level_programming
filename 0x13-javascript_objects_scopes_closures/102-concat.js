ef1c485c21fd3a4fe8fc9838621cf12c36ff7b78
AropetJ <aropetjoel32@gmail.com>
2023-09-12 15:44:04 +0300

    Attempted task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,44 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+const sourceFilePath1 = process.argv[2];
+const sourceFilePath2 = process.argv[3];
+const destinationFilePath = process.argv[4];
+
+if (!sourceFilePath1 || !sourceFilePath2 || !destinationFilePath) {
+  console.error('Usage: ./102-concat.js sourceFile1 sourceFile2 destinationFile');
+  process.exit(1);
+}
+
+const readStream1 = fs.createReadStream(sourceFilePath1);
+const writeStream = fs.createWriteStream(destinationFilePath, { flags: 'a' });
+
+readStream1.pipe(writeStream);
+
+writeStream.on('finish', () => {
+  const readStream2 = fs.createReadStream(sourceFilePath2);
+
+  readStream2.pipe(writeStream);
+
+  readStream2.on('end', () => {
+    readStream1.close();
+    readStream2.close();
+    writeStream.close();
+    console.log('Concatenation complete.');
+  });
+});
+
+readStream1.on('error', (err) => {
+  console.error(`Error reading source file 1: ${err.message}`);
+  process.exit(1);
+});
+
+readStream2.on('error', (err) => {
+  console.error(`Error reading source file 2: ${err.message}`);
+  process.exit(1);
+});
+
+writeStream.on('error', (err) => {
+  console.error(`Error writing to destination file: ${err.message}`);
+  process.exit(1);
+});
