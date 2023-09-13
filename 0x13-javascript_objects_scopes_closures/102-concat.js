9a5e04647a05d989dabb18d722cbaaa71e79a896
AropetJ <aropetjoel32@gmail.com>
2023-09-13 14:43:00 +0300

    completed task 102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,44 +1,35 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+
+if (process.argv.length !== 5) {
+  console.error('Usage: ./102-concat.js <fileA> <fileB> <fileC>');
+  process.exit(1);
+}
+
 const sourceFilePath1 = process.argv[2];
 const sourceFilePath2 = process.argv[3];
 const destinationFilePath = process.argv[4];
 
-if (!sourceFilePath1 || !sourceFilePath2 || !destinationFilePath) {
-  console.error('Usage: ./102-concat.js sourceFile1 sourceFile2 destinationFile');
-  process.exit(1);
-}
+fs.readFile(sourceFilePath1, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
-const readStream1 = fs.createReadStream(sourceFilePath1);
-const writeStream = fs.createWriteStream(destinationFilePath, { flags: 'a' });
+  fs.readFile(sourceFilePath2, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
 
-readStream1.pipe(writeStream);
+    const concatenatedContent = data1 + data2;
 
-writeStream.on('finish', () => {
-  const readStream2 = fs.createReadStream(sourceFilePath2);
-
-  readStream2.pipe(writeStream);
-
-  readStream2.on('end', () => {
-    readStream1.close();
-    readStream2.close();
-    writeStream.close();
-    console.log('Concatenation complete.');
+    fs.writeFile(destinationFilePath, concatenatedContent, (err) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+    });
   });
-});
-
-readStream1.on('error', (err) => {
-  console.error(`Error reading source file 1: ${err.message}`);
-  process.exit(1);
-});
-
-readStream2.on('error', (err) => {
-  console.error(`Error reading source file 2: ${err.message}`);
-  process.exit(1);
-});
-
-writeStream.on('error', (err) => {
-  console.error(`Error writing to destination file: ${err.message}`);
-  process.exit(1);
 });
