a45a150a6c913551257024ab9eac7cf010554ed1
AropetJ <aropetjoel32@gmail.com>
2023-09-18 03:21:21 +0300

    Completed task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if len(sys.argv) != 4:
+    print("Usage: {} <username> <password> <database>"
+          .format(sys.argv[0]))
+    sys.exit(1)
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+
+try:
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = connection.cursor()
+
+    query = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id
+    """
+
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    cursor.close()
+    connection.close()
+
+except MySQLdb.Error as e:
+    print("MySQL Error:", e)
+    sys.exit(1)
