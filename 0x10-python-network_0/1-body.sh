b2729d3a3272dcc3079ddbe4313d5e9862afc08e
AropetJ <aropetjoel32@gmail.com>
2023-09-28 21:05:17 +0300

    Attempted task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to a URL and displays the body of the response for a 200 status code
-curl -s "$1" | awk 'NR==1 {if ($2 == 200) print}' | sed '1d'
+curl -s -w "\n%{http_code}" "$1" | awk 'END{if($1 == "200") print $0}' RS='\n\n'
