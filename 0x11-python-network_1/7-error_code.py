a1845580c2fbbe52b7b316353e913d66c9d32bae
AropetJ <aropetjoel32@gmail.com>
2023-09-30 01:00:51 +0300

    Attempted task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,11 +5,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
     response = requests.get(url)
-    content = response.text
 
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
-        print(content)
+        print(response.text)
