58117af732c4c121d4fc0bbe20add9b36ccd01ff
AropetJ <aropetjoel32@gmail.com>
2023-09-30 00:54:37 +0300

    Attempted task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# Python script that fetches https://alx-intranet.hbtn.io/status
+"""Python script that fetches https://alx-intranet.hbtn.io/status.
+"""
 import requests
 
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    response = requests.get(url)
 
+if __name__ == "__main__":
+    resp = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(resp.text)))
+    print("\t- content: {}".format(resp.text))
