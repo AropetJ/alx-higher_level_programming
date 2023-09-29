a37c116da4f04ecf3e02c80cdd698fba80ba537f
AropetJ <aropetjoel32@gmail.com>
2023-09-30 00:42:58 +0300

    Attempted task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
+"""fetches https://alx-intranet.hbtn.io/status.
 """
-
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
