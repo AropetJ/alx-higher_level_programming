38f1a812be12ff8e4d0236b3f73946c5462c0495
AropetJ <aropetjoel32@gmail.com>
2023-09-30 01:02:45 +0300

    Attempted task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-# Python script that takes in a URL, sends a request
+"""Python script that takes in a URL, sends a request
+"""
 import requests
 import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
