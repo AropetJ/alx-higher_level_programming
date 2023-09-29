3d4e45c3b692bda533653f7edebee62e672531fb
AropetJ <aropetjoel32@gmail.com>
2023-09-29 23:18:20 +0300

    Attempted task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Python script that takes your GitHub credentials (username
+   and password) and uses the GitHub API to display your id
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        response = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
+        print(response.json().get('id'))
+    except:
+        print("None")
