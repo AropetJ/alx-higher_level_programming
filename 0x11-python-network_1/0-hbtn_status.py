d27be7e01940047ecf18dd7a07c1f5db6240b65d
AropetJ <aropetjoel32@gmail.com>
2023-09-30 00:08:32 +0300

    Attempted task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         content = response.read()
         utf8_content = content.decode('utf-8')
-        print('Body response:')
-        print('\t- type: {}'.format(type(content)))
-        print('\t- content: {}'.format(content))
-        print('\t- utf8 content: {}'.format(utf8_content))
+        typed = response.info()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(utf8_content))
